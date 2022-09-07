f75a6e66e5e9c04dded857c27c3d4c139b6aedfd
Mursu <mursujesse@gmail.com>
2022-09-07 15:21:53 +0300

    7.1

---
+++ 7. Monikko, joukko ja sanakirja/7.2.py
@@ -0,0 +1,12 @@
+names = set()
+name = input("Anna nimi")
+while name != "":
+    if name in names:
+        print("Aiemmin syötetty nimi")
+    else:
+        print("Uusi nimi")
+        names.add(name)
+    name = input("Anna nimi")
+
+for i in names:
+    print(i)
