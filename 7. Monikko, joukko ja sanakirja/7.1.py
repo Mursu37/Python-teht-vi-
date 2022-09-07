86a7f9df62784bbef73d13aa717ba8dab236340e
Mursu <mursujesse@gmail.com>
2022-09-07 15:02:00 +0300

    7.1

---
+++ 7. Monikko, joukko ja sanakirja/7.1.py
@@ -0,0 +1,11 @@
+kuukaudet = ("tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu")
+jarjestysluku = int(input("Anna kuukauden numero"))
+kuukausi = jarjestysluku - 1
+if kuukausi < 2 or kuukausi == 11:
+    print("talvi")
+elif kuukausi < 5:
+    print("kevät")
+elif kuukausi < 8:
+    print("kesä")
+elif kuukausi < 11:
+    print("syksy")
