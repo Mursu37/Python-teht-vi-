fb6d5f26361e7328e94a6ccdb60b178951eb08ce
Mursu <mursujesse@gmail.com>
2022-09-08 15:00:00 +0300

    bugfix

--- 7. Monikko, joukko ja sanakirja/7.1.py
+++ 7. Monikko, joukko ja sanakirja/7.1.py
@@ -1,10 +1,11 @@
 months = ("tammikuu", "helmikuu", "maaliskuu",
-             "huhtikuu", "toukokuu","kesäkuu",
-             "heinäkuu", "elokuu", "syyskuu",
-             "lokakuu", "marraskuu", "joulukuu")
+          "huhtikuu", "toukokuu","kesäkuu",
+          "heinäkuu", "elokuu", "syyskuu",
+          "lokakuu", "marraskuu", "joulukuu")
 userInput = int(input("Anna kuukauden numero"))
 month = userInput - 1
 print(months[month])
+
 if month < 2 or month == 11:
     print("talvi")
 elif month < 5:
