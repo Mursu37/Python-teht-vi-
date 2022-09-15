240a96f4ce8acd0db17613f853f7c0daef1e6f19
Mursu <mursujesse@gmail.com>
2022-09-16 01:05:31 +0300

    9.4 class luotu

---
+++ 9. Luokka, olio, alustaja/9.4.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, reg, top_speed, current_speed=0, distance=0):
+        self.reg = reg
+        self.top_speed = top_speed
+        self.current_speed = current_speed
+        self.distance = distance
