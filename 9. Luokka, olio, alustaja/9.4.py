ab27d2afded0b90b020c6874814154b652cadf2f
Mursu <mursujesse@gmail.com>
2022-09-26 03:36:43 +0300

    9.4

--- 9. Luokka, olio, alustaja/9.4.py
+++ 9. Luokka, olio, alustaja/9.4.py
@@ -1,6 +1,46 @@
+import random
+
+
 class Car:
     def __init__(self, reg, top_speed, current_speed=0, distance=0):
         self.reg = reg
         self.top_speed = top_speed
         self.current_speed = current_speed
         self.distance = distance
+
+    def accelerate(self, acceleration):
+        if acceleration < 0:
+            if self.current_speed + acceleration < 0:
+                self.current_speed = 0
+            else:
+                self.current_speed += acceleration
+
+        elif self.current_speed + acceleration > self.top_speed:
+            self.current_speed = self.top_speed
+
+        else:
+            self.current_speed += acceleration
+        return self.current_speed
+
+    def move(self, hours):
+        self.distance = self.distance + self.current_speed * hours
+
+
+competing_cars = []
+for count in range(10):
+    car = Car("ABC-" + str(count), random.randint(100, 200))
+    competing_cars.append(car)
+
+race_not_over = True
+while race_not_over:
+    for cars in competing_cars:
+        cars.accelerate(random.randint(-10, 15))
+        cars.move(1)
+
+    for cars in competing_cars:
+        if cars.distance >= 10000:
+            race_not_over = False
+
+
+for cars in competing_cars:
+    print(f"{cars.reg}, {cars.top_speed}, {cars.current_speed}, {cars.distance}")
